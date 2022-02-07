4870711cfac35cba9569a1fe8ccb019dcb3bdb46
Zeleboba441 <89891909+Zeleboba441@users.noreply.github.com>
2022-02-07 11:20:29 +1000

    Add files via upload

---
+++ WpfApp09/WpfApp09/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp09
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public People EditPeople { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            EditPeople = new People { Name = "Наталья Виллер", State = 228 };
+            DataContext = this;
+        }
+
+        private void Save(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click1(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://i.gifer.com/AwJO.mp4");
+        }
+    }
+}
---
+++ WpfApp09/WpfApp09/People.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp09
+{
+    public class People : IDataErrorInfo
+    {
+        public string this[string columnName]
+        {
+            get
+            {
+                string errorInfo = null;
+                switch (columnName)
+                {
+                    case "State":
+                        if (State > 228)
+                            errorInfo = "Введена не правильная статья XD XD " +
+                                "Вы депортированные в КОРЕЮ";
+                        else if (State < 0)
+                            errorInfo = "Статья не может быть меньше нуля XD XD XD XD ";
+                        break;
+                    case "Name":
+                        if (Name.Length < 4)
+                            errorInfo = "Имя слишком короткое";
+                        break;
+                   
+                }
+                return errorInfo;
+            }
+        }
+
+        public string Name { get; set; }
+        public int State { get; set; }
+        public string Error { get; set; }
+    }
+}
